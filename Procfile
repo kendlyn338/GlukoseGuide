ac562caa43edf9efabe06d6814524cf88a73c6e4
Kendlyn Marriott <kendlynmarriott@Kendlyns-iMac.attlocal.net>
2022-03-03 09:39:13 -0500

    config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn glukoseguide.wsgi
--- glukoseguide/settings.py
+++ glukoseguide/settings.py
@@ -128,3 +128,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
